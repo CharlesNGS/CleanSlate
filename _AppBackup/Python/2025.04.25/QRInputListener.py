2290985e9dc34af7653cc2c7dbc8ee7a634b1fbf
CharlesNGS <charles.nameless.seaman@gmail.com>
2025-04-26 00:22:32 +0700

    Add .gitignore file

--- _AppBackup/Python/2025.04.25/QRInputListener.py
+++
@@ -1,8 +0,0 @@
-#Imported functions.
-from QRValidator import completeQuery
-
-#Hash is the variable that queries the database.
-QRHashInput = input("qrHash")
-
-#Each received input runs the inported function. 
-completeQuery(QRHashInput)
